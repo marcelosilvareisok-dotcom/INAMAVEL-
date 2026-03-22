296ba5dd70bc8152aa141958b3199cca43069352
marcelosilvareisok-dotcom <marcelosilvareisok@gmail.com>
2026-03-22 15:43:08 -0300

    feat: Update Firebase config placeholders
    Replaces hardcoded Firebase VAPID keys with TODO placeholders in the service worker and notifications service. This prompts users to configure their own Firebase project credentials.
    
    Also, updates the Vite dependency to ^6.2.0 and introduces `overrides` for React dependencies in `package.json` to manage potential version conflicts.

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -8,7 +8,7 @@ importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-messaging-comp
 // For now, we'll use a placeholder that the user can update or we can try to automate.
 
 firebase.initializeApp({
-  apiKey: "2cuSd9TiFsq0hjQ4aW6Lk55HK9x_FpyaqEeQODaF1Vg",
+  apiKey: "TODO_API_KEY",
   authDomain: "TODO_AUTH_DOMAIN",
   projectId: "TODO_PROJECT_ID",
   storageBucket: "TODO_STORAGE_BUCKET",
